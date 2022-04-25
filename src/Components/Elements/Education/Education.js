5d9e95af3215eeef3bcaf1aa69a84491e1ff611c
RayFlaherty <90649331+RayFlaherty@users.noreply.github.com>
2022-04-25 14:01:09 -0500

    adding police stuff

--- src/Components/Elements/Education/Education.js
+++ src/Components/Elements/Education/Education.js
@@ -7,15 +7,31 @@ const Education = () => {
     <Card>
       <div className="h1Header">
         <h2 className="h1Text"> Work History and Education </h2>
-        <div >
+        <div>
           <div className="certDisplay">
             <Card className="certStyle smallCertStyleWidth">
               <h3>Graduate of Vanderbilt</h3>
               <p> Enrolled September of 2021</p>
               <p> Completed Program March 2022</p>
             </Card>
-            <Card className="certStyle right">
+            <Card className="certStyle">
               <img src={VandyCert} className="largeCertStyleWidth"></img>
+            </Card>
+          </div>
+          <div className="certDisplay">
+            <Card className="certStyle largeCertStyleWidth">
+              <h3>Completion Expected in May 2022</h3>
+            </Card>
+            <Card className="certStyle certStyle smallCertStyleWidth">
+                <h3>Complete React Course</h3>
+                <p> Course Provided by Udemy </p>
+            </Card>
+          </div>
+          <div className="certDisplay">
+          <Card className="certStyle smallCertStyleWidth">
+              <h3>Metropolitan Nashville Police Department Academy</h3>
+              <p> Enrolled March of 2011</p>
+              <p> Completed Program September 2011</p>
             </Card>
           </div>
         </div>
